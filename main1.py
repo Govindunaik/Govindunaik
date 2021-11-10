55ccd939314946fc8f32b9c927223a2753f7e3e7
sreegoth <gothsree@gmail.com>
2021-11-10 15:30:24 +0530

    print updated

--- main1.py
+++ main1.py
@@ -3,6 +3,6 @@ p=n**(1/2)
 for i in range(2):
 	m=n/p
 if(m==p):
-	print("yes, given no is perfect square")
+	print("yes, given no is perfect squre")
 else:
-	print("not a square")
+	print("not a squre")
